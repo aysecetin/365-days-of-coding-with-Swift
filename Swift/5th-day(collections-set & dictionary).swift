92d0fd9e02d9e1ee585b91a4655469141231c0b7
Ayşe Çetin <122305175+aysecetin@users.noreply.github.com>
2024-08-20 21:43:26 +0300

    Update 5th-day(collections-set & dictionary).swift
    I tried to explain the set structure with examples

--- Swift/5th-day(collections-set & dictionary).swift
+++ Swift/5th-day(collections-set & dictionary).swift
@@ -1,4 +1,4 @@
-
+import UIKit
 // SET
 // Has the same properties as Array.
 // You cannot save the same data repeatedly
@@ -31,4 +31,23 @@ fruits.max()
 fruits.min()
 
 fruits.removeAll()
+
+//set examples
+
+let odds:Set = [1,3,5,7,9]
+let evens:Set = [0,2,4,6,8]
+let primes:Set = [2,3,5,7]
+
+let array = odds.union(evens).sorted()
+print(array)
+
+let array1 = odds.intersection(evens).sorted() /// gets intersections
+print(array1)
+
+let array2 = odds.subtracting(primes) ///subtracting primes from singles
+print(array2)
+
+let array3 = odds.symmetricDifference(primes).sorted() /// extracts intersections from all
+print(array3)
+
 
