43e98f6b700d6c735162312bc0b05ed9c8485457
Ayşe Çetin <122305175+aysecetin@users.noreply.github.com>
2024-08-20 22:11:29 +0300

    Update 5th-day(collections-set & dictionary).swift

--- Swift/5th-day(collections-set & dictionary).swift
+++ Swift/5th-day(collections-set & dictionary).swift
@@ -50,4 +50,69 @@ print(array2)
 let array3 = odds.symmetricDifference(primes).sorted() /// extracts intersections from all
 print(array3)
 
+//DICTIONARY 
+
+/*
+- The empty dictionary is represented by ‘[:] ’.
+- It is the same as the ‘hash - map’ structure in Java
+- key and value are related
+*/
+
+var dict1 = [Int:String]()
+
+var dict2 = [3.14:"pi", 2.71:"e"]
+
+var dict3:[Int:String] = [1:"bir", 2:"iki", 3:"üç"]
+
+var iller = [16: "bursa", 34:"istanbul", 6:"ankara"]
+print(iller)
+
+iller[35] = "izmir"
+iller[7] = "antalya"
+print(iller)
+
+iller[16] = "yeni bursa"
+print(iller)
+
+iller.updateValue("yeni izmir", forKey: 35)
+print(iller)
+
+for (anahtar,değer) in iller{
+    print("Anahtar:\(anahtar), il:\(değer)")
+}
+
+iller.removeValue(forKey: 35)
+print(iller)
+
+iller.isEmpty
+iller.count
+iller.first
+
+var ters = iller.reversed()
+print(ters)
+
+
+
+//**JOINING SEQUENCES AND CREATING A DICTIONARY**
+
+var courses = ["chemistry", "maths", "literature"]
+var grades = [50,80,60]
+
+var gradesOfCourses = Dictionary(uniqueKeysWithValues: zip(courses,grades))
+print(gradesOfCourses)
+
+
+////**SORTING THE DICTIONARY INTO SERIES**
+
+var pricesOfProduct:[Double:String] = [15.99: "book", 299.99: "t-shirt", 32.49: "watch" ]
+
+var prices = [Double](pricesOfProduct.keys)
+var products = [String](pricesOfProduct.values)
+print(prices)
+print(products)
+
+
+
+
+
 
